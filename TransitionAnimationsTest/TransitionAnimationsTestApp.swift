8551621fbc5be1b877c9f891a74322adcd9fab49
Oleg R <oleg.r@thrive.io>
2022-02-02 15:57:27 +0200

    Initial Commit

---
+++ TransitionAnimationsTest/TransitionAnimationsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionAnimationsTestApp.swift
+//  TransitionAnimationsTest
+//
+//  Created by Oleg on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TransitionAnimationsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
